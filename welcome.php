b699b8958bcbc9a343f54d0eaef7ad976085f51c
heyitssat <satyamss2497@gmail.com>
2016-06-01 05:04:31 +0530

    Add files via upload
    database check

---
+++ welcome.php
@@ -0,0 +1,22 @@
+﻿<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli("MySQL Server 5.7.12", "codesat", "S@tcode");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
